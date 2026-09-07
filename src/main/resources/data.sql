9f5119186b51744b1a65903a3b8ae398d69ba65d
gevg14 <gerardoveragonzalez57@gmail.com>
2026-09-07 03:05:27 -0300

    productos agregados y modificaciones

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO productos (nombre, descripcion, precio, stock, categoria) 
+VALUES ('Laptop Gamer PRO', 'Intel i7 13ª Gen, 16GB RAM, RTX 4060', 999990.00, 10, 'Tecnología');
+
+INSERT INTO productos (nombre, descripcion, precio, stock, categoria) 
+VALUES ('Mouse Inalámbrico RGB', 'Sensor óptico 16000 DPI ergonómico', 35990.00, 50, 'Accesorios');
+
+INSERT INTO productos (nombre, descripcion, precio, stock, categoria) 
+VALUES ('Teclado Mecánico Switch Red', 'Formato 75% con iluminación RGB', 59990.00, 30, 'Accesorios');
+
+INSERT INTO productos (nombre, descripcion, precio, stock, categoria) 
+VALUES ('Monitor Gaming 27 IPS', '165Hz 1ms FHD con FreeSync', 189990.00, 15, 'Pantallas');Ñ
